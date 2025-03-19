55cf74b54204875e7667069723314b88cdc6fa2f
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-19 13:59:27 -0400

    acting on events done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", handleKeyDown);
+
+let xAxis = 180;
+let yAxis = 0;
+
+function moveDodgerLeft() {
+  xAxis--;
+  dodger.style.left = xAxis + "px";
+}
+
+function moveDodgerRight() {
+  xAxis++;
+  dodger.style.left = xAxis + "px";
+}
+
+function handleKeyDown(e) {
+  const { key } = e;
+  switch (key) {
+    case "ArrowRight":
+      moveDodgerRight();
+      break;
+    case "ArrowLeft":
+      moveDodgerLeft();
+      break;
+    case "ArrowUp":
+      yAxis++;
+      dodger.style.bottom = yAxis + "px"; 
+      break;
+    case "ArrowDown":
+      yAxis--;
+      dodger.style.bottom = yAxis + "px";
+      break;
+  }
+}
